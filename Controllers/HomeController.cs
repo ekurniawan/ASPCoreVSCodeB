b84139568ba3fef38b091fc9da38246260b6f574
ekurniawan <erick.kurniawan@gmail.com>
2019-03-11 15:00:01 +0700

    menambahkan controller Contact

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,17 @@
 
+//localhost/{Controller}/{Action Method}
 public class HomeController
 {
-    //action result
+    //action method
     public string Index(){
         return "Hello from ASP Controller";
     }
 
     public string About(){
         return "Ini adalah halaman About";
+    }
+
+    public string Contact(){
+        return "Ini adalah halaman Contact";
     }
 }
