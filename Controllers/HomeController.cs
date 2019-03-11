f8af4ba79474af902b509891a6a8b8efa44672bd
ekurniawan <erick.kurniawan@gmail.com>
2019-03-11 14:51:01 +0700

    menambahkan controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,12 @@
 
-public class HomeController{
+public class HomeController
+{
+    //action result
     public string Index(){
         return "Hello from ASP Controller";
+    }
+
+    public string About(){
+        return "Ini adalah halaman About";
     }
 }
