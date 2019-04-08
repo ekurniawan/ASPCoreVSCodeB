25944146bd1a0f9810f7eb659a9f7ed940e2dcbe
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-04-08 15:36:22 +0700

    menambahkan interface mahasiswa

--- DAL/MahasiswaDAL.cs
+++ DAL/MahasiswaDAL.cs
@@ -4,19 +4,43 @@ using ASPCoreGroupB.Models;
 using Microsoft.Extensions.Configuration;
 
 namespace ASPCoreGroupB.DAL { 
-    public class MahasiswaDAL {
+    public class MahasiswaDAL : IMahasiswa {
         private IConfiguration _config;
         public MahasiswaDAL(IConfiguration config)
         {
             _config = config;
         }
 
-        
+        private string GetConnStr(){
+            return _config.GetConnectionString("DefaultConnection");
+        }
 
         public List<Mahasiswa> GetAll(){
             using(SqlConnection conn = new SqlConnection()){
 
             }
+        }
+
+        public void Delete(string nim)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        
+
+        public Mahasiswa GetById(string nim)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert(Mahasiswa mhs)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Mahasiswa mhs)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
