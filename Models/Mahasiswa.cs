982d864ec27c01e104081090a42b5b7b992e4d1a
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-05-06 15:10:22 +0700

    menambahkan validasi

--- Models/Mahasiswa.cs
+++ Models/Mahasiswa.cs
@@ -1,8 +1,16 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace ASPCoreGroupB.Models{
     public class Mahasiswa {
+
+        [Required]
         public string Nim { get; set; }
+        
+        [Required]
         public string Nama { get; set; }
+        
+        [Required]
         public string Email { get; set; }
         public string Telp { get; set; }
     }
