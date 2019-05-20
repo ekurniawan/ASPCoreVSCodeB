d156b6ef84878d00f4cfbeb81ae52971523ce51a
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-05-20 15:23:27 +0700

    menambahkan menu

--- Models/Pengguna.cs
+++ Models/Pengguna.cs
@@ -1,10 +1,23 @@
-
+using System.ComponentModel.DataAnnotations;
 namespace ASPCoreGroupB.Models{
     public class Pengguna {
+        [Required]
         public string Username { get; set; }
+        
+        [Required]
         public string Password { get; set; }
+        
+        [Required]
+        [Compare("Password")]
+        public string Repassword { get; set; }
+        
+        [Required]
         public string Aturan { get; set; }  
+        
+        [Required]
         public string Nama { get; set; }
+        
+        [Required]
         public string Telp { get; set; }
     }
 }
